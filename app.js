61503fbaf3707f4ef3421a7eedf5168380819e5d
PAWAN KUMAR <121pawank@gmail.com>
2020-09-12 00:04:12 +0530

    updated stories editor, helper for date formate

--- app.js
+++ app.js
@@ -18,13 +18,30 @@ require("./config/passport")(passport);
 connectDB();
 const app = express();
 
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 // Logging
 if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
 }
 
+// Handlebars helpers
+
+const { formatDate } = require("./helpers/hbs");
+
 // Handlebars
-app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.engine(
+    ".hbs",
+    exphbs({
+        helpers: {
+            formatDate,
+        },
+        defaultLayout: "main",
+        extname: ".hbs",
+    })
+);
 app.set("view engine", ".hbs");
 
 // Sessions
