34cc8947d3b828734172b03a1563d978f8f39800
PAWAN KUMAR <121pawank@gmail.com>
2020-09-13 21:45:19 +0530

    added react component

--- config/passport.js
+++ config/passport.js
@@ -1,41 +1,44 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
-
 const User = require("../models/User");
+const HttpsProxyAgent = require("https-proxy-agent");
 
 module.exports = function (passport) {
-    passport.use(
-        new GoogleStrategy(
-            {
-                clientID: process.env.GOOGLE_CLIENT_ID,
-                clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                callbackURL: "/auth/google/callback",
-            },
-            async (accessToken, refreshToken, profile, done) => {
-                console.log(profile);
-                const newUser = {
-                    googleId: profile.id,
-                    displayName: profile.displayName,
-                    firstName: profile.name.givenName,
-                    lastName: profile.name.familyName,
-                    image: profile.photos[0].value,
-                };
+    const gStrategy = new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: "/auth/google/callback",
+        },
+        async (accessToken, refreshToken, profile, done) => {
+            console.log(profile);
+            const newUser = {
+                googleId: profile.id,
+                displayName: profile.displayName,
+                firstName: profile.name.givenName,
+                lastName: profile.name.familyName,
+                image: profile.photos[0].value,
+            };
 
-                try {
-                    let user = await User.findOne({ googleId: profile.id });
+            try {
+                let user = await User.findOne({ googleId: profile.id });
 
-                    if (user) {
-                        done(null, user);
-                    } else {
-                        user = await User.create(newUser);
-                        done(null, newUser);
-                    }
-                } catch (error) {
-                    console.log(error);
+                if (user) {
+                    done(null, user);
+                } else {
+                    user = await User.create(newUser);
+                    done(null, newUser);
                 }
+            } catch (error) {
+                console.log(error);
             }
-        )
+        }
     );
+
+    // const agent = new HttpsProxyAgent(process.env.HTTP_PROXY || "http://157.45.52.221:3000");
+    // gStrategy._oauth2.setAgent(agent);
+
+    passport.use(gStrategy);
 
     passport.serializeUser((user, done) => done(null, user.id));
 
